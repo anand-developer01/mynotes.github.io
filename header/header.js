9fbdb27626a3cfe4a871b46030a54b2b9db96f9d
anand <p.anand.developer@gmail.com>
2024-07-16 02:31:26 +0530

    game re commited again

--- header/header.js
+++ header/header.js
@@ -1,8 +1,9 @@
+
 const navHeader = `
 <input type="checkbox" id="nav-check">
 <div class="nav-header">
     <div class="nav-title">
-        Logo
+    <a href="../">Logo</a>
     </div>
 </div>
 <div class="nav-btn">
@@ -18,11 +19,10 @@ const navHeader = `
     <li><a href="./javascript-notes.html">Javascript</a></li>
     <li><a href="react-notes.html"> React </a></li>
     <li><a href="vue-notes.html">Vue</a></li>
-    <li><a href="typescript-notes.html">TypeScript</a></li>
+    <li><a href="#">TypeScript</a></li>
     <li><a href="#">css & bootstrap</a></li>
     <li><a href="./graphql-notes.html">GraphQL</a></li>
     <li><a href="#">redux</a></li>
     <li><a href="#">VS</a></li>
     <li><a href="./js-code-challenges.html">Code Challenges</a></li>
-    <li><a href="./english.html">English</a></li>
 </ul>`
