802961d51efcdef696e14b8b3301073ca1705332
anand-developer01 <67739417+anand-developer01@users.noreply.github.com>
2024-09-24 01:34:54 +0530

    Update python-notes.js

--- notes-data/python-notes.js
+++ notes-data/python-notes.js
@@ -562,12 +562,55 @@ else:
         {
             id: 1,
             section: `Listes`,
-            title: "What is Python?",
+            title: "Listes in Python",
             note: [
                 {
-                    text1: `What is Python?`,
+                    text1: `Listes`,
                     code1: ``
-                }
+                },
+                {
+                    text1: `
+                    
+                    <div class='table-res'>
+                    <table border=1 >
+<tbody><tr>
+<th>Method</th>
+<th>Description</th>
+</tr>
+<tr><td>append()</td><td>Adds an element at 
+  the end of the list</td></tr>
+<tr><td>clear()</td><td>Removes all the 
+  elements from the list</td></tr>
+<tr><td>copy()</td><td>Returns a copy of the 
+  list</td></tr>
+<tr><td>count()</td><td>Returns the number of 
+  elements with the specified value</td></tr>
+<tr><td>extend()</td><td>Add the elements of a 
+  list (or any iterable), to the end of the current list</td></tr>
+<tr><td>index()</td><td>Returns the index of 
+  the first element with the specified value</td></tr>
+<tr><td>insert()</td><td>Adds an element at 
+  the specified position</td></tr>
+<tr><td>pop()</td><td>Removes the element at the 
+  specified position</td></tr>
+<tr><td>remove()</td><td>Removes the first 
+  item with the specified value</td></tr>
+<tr><td>reverse()</td><td>Reverses the order 
+  of the list</td></tr>
+<tr><td>sort()</td><td>Sorts the list</td></tr>
+</tbody></table>
+</div>
+`,
+                    code1: ``
+                },                
+                {
+                    text1: `Listes`,
+                    code1: ``
+                },                
+                {
+                    text1: `Listes`,
+                    code1: ``
+                },
             ]
         },
         {
